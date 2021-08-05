7dae8b00809389853e3b306126e7b250cc78b979
Kitsune-fx <kant.cmd@gmail.com>
2021-08-05 13:54:19 +0700

    Week 1 to 5

---
+++ lab1/Countdown.java
@@ -0,0 +1,16 @@
+public class Countdown{
+    //-----------------------------------------------------------------
+    //program by Kan 642115003
+    //-----------------------------------------------------------------
+
+    public static void main(String[] args) {
+        System.out.print("Three....");
+        System.out.print("Two....");
+        System.out.print("One....");
+        System.out.print("Zero....");
+
+        System.out.println("2010");
+        System.out.println("This is My First JAVA program :");
+    }
+
+}
